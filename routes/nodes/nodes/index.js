a4d451f028d118d5c9990495abace4207cfed5f2
William Niemiec <williamniemiec@hotmail.com>
2022-08-09 21:09:11 -0300

    Conditional

--- routes/nodes/nodes/index.js
+++ routes/nodes/nodes/index.js
@@ -1,9 +1,11 @@
 const nodes = [
-  require('./old/dataInput.json'),
-  require('./old/externalLink.json'),
-  require('./old/motivational.json'),
-  require('./old/reminder.json'),
-  require('./old/timeTicker.json')
+  // require('./old/dataInput.json'),
+  // require('./old/externalLink.json'),
+  // require('./old/motivational.json'),
+  // require('./old/reminder.json'),
+  // require('./old/timeTicker.json')
+  require ('./begin.json'),
+  require ('./conditional.json'),
 ];
 
 module.exports = nodes;
