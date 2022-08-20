1016d486790827ff0e860b16cab46384988c88d3
William Niemiec <williamniemiec@hotmail.com>
2022-08-20 09:41:10 -0300

    Refactoring

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -3,114 +3,106 @@ const flowService = require('../../../services/flow.service');
 const edgeService = require('../../../services/edge.service');
 const ag =  require('../../helpers/jobQueue');
 
-const getAll = async (req: any, res: any, next: any) => {
-    const user = await req.user;
-    const flows = await flowService.findAllByUserId(user.id);
-
-    res.send(flows);
-};
-
-const get = async (req: any, res: any, next: any) => {
-    const user = await req.user;
-    const flowId = await req.params.uid;
-
-    const flow = await flowService.findAllByUserIdAndFlowId(user.id, flowId);
-    const nodes = await nodeService.findAllByFlowId(flowId);
-    const edges = await edgeService.findAllByFlowId(flowId);
-
-    res.send({
-        flowId: flow.id,
-        flowName: flow.name,
-        flowDescription: flow.description,
-        flowEmail: flow.userEmail,
-        data: [
-            ...nodes,
-            ...edges
-        ]
-   });
-};
-
-const create = async (req: any, res: any, next: any) => {
-    const { data, name, description, userEmail } = req.body
-    const user = await req.user;
-
-    let timeTickers = [];
-    let nodes = data.filter((d: { type: any; }) => d.type);
-    let edges = data.filter((d: { source: any; }) => d.source);
-
-    const flow = await flowService.insert({
-        user: user.id,
-        name: name,
-        userEmail: userEmail,
-        description: description
-    });
-
-    for (let n of nodes){
-        console.log('STORING', n.id)
-        const storedNode = await nodeService.insert({ type: n.type, position: n?.position, data: n?.data, flow: flow.id, id: n.id });
-
-        edges.map((e: { target: any; source: any; }) => {
-            if(e?.target === n.id){
-                e.target = storedNode.id
-            }
-            if(e.source === n.id){
-                e.source = storedNode.id
-            }
-        });
-
-        if(storedNode.type === 'TIME_TICKER'){
-            timeTickers.push(storedNode);
-        }
-    }
-    console.log('inserting edges...');
-    for(let e of edges){
-        edgeService.insert({source: e.source, target: e?.target, flow: flow.id, animated: e.animated});
-    }
-
-    console.log('ok');
-
-
-    for(let t of timeTickers){
-        
-
-        if (t.data.results === undefined) {
-            t.data.results = {
-                repeatInterval: "0",
-                skipDays: "0"
-            };
-        }
-
-        const agendaData = t.data.results;
-
-        const job = ag.ag.create("TIME_TICKER", t);
-
-        job.repeatEvery(agendaData.repeatInterval, {
-            endDate: new Date(agendaData.endDate),
-            skipDays: agendaData.skipDays,
-            skipImmediate: agendaData.skipImmediate,
-            startDate: new Date(agendaData.startDate),
-        });
-        job.save();
-    }
-
-    res.send('Success');
-};
-
-const remove = async (req: any, res: any, next: any) => {
-    const user = await req.user;
-    const flowId = await req.params.uid;
-
-    const flow = await flowService.removeWithUserIdAndFlowId(user.id, flowId);
-    await nodeService.removeAllWithFlowId(flow.id);
-    await edgeService.removeAllWithFlowId(flow.id);
-
-
-    res.send('success');
-};
-
 module.exports = {
-    getAll,
-    get,
-    remove,
-    create
+    getAll: async (req: any, res: any, next: any) => {
+      const user = await req.user;
+      const flows = await flowService.findAllByUserId(user.id);
+  
+      res.send(flows);
+    },
+    get: async (req: any, res: any, next: any) => {
+      const user = await req.user;
+      const flowId = await req.params.uid;
+  
+      const flow = await flowService.findAllByUserIdAndFlowId(user.id, flowId);
+      const nodes = await nodeService.findAllByFlowId(flowId);
+      const edges = await edgeService.findAllByFlowId(flowId);
+  
+      res.send({
+          flowId: flow.id,
+          flowName: flow.name,
+          flowDescription: flow.description,
+          flowEmail: flow.userEmail,
+          data: [
+              ...nodes,
+              ...edges
+          ]
+     });
+    },
+    remove: async (req: any, res: any, next: any) => {
+      const user = await req.user;
+      const flowId = await req.params.uid;
+  
+      const flow = await flowService.removeWithUserIdAndFlowId(user.id, flowId);
+      await nodeService.removeAllWithFlowId(flow.id);
+      await edgeService.removeAllWithFlowId(flow.id);
+  
+  
+      res.send('success');
+    },
+    create: async (req: any, res: any, next: any) => {
+      const { data, name, description, userEmail } = req.body
+      const user = await req.user;
+  
+      let timeTickers = [];
+      let nodes = data.filter((d: { type: any; }) => d.type);
+      let edges = data.filter((d: { source: any; }) => d.source);
+  
+      const flow = await flowService.insert({
+          user: user.id,
+          name: name,
+          userEmail: userEmail,
+          description: description
+      });
+  
+      for (let n of nodes){
+          console.log('STORING', n.id)
+          const storedNode = await nodeService.insert({ type: n.type, position: n?.position, data: n?.data, flow: flow.id, id: n.id });
+  
+          edges.map((e: { target: any; source: any; }) => {
+              if(e?.target === n.id){
+                  e.target = storedNode.id
+              }
+              if(e.source === n.id){
+                  e.source = storedNode.id
+              }
+          });
+  
+          if(storedNode.type === 'TIME_TICKER'){
+              timeTickers.push(storedNode);
+          }
+      }
+      console.log('inserting edges...');
+      for(let e of edges){
+          edgeService.insert({source: e.source, target: e?.target, flow: flow.id, animated: e.animated});
+      }
+  
+      console.log('ok');
+  
+  
+      for(let t of timeTickers){
+          
+  
+          if (t.data.results === undefined) {
+              t.data.results = {
+                  repeatInterval: "0",
+                  skipDays: "0"
+              };
+          }
+  
+          const agendaData = t.data.results;
+  
+          const job = ag.ag.create("TIME_TICKER", t);
+  
+          job.repeatEvery(agendaData.repeatInterval, {
+              endDate: new Date(agendaData.endDate),
+              skipDays: agendaData.skipDays,
+              skipImmediate: agendaData.skipImmediate,
+              startDate: new Date(agendaData.startDate),
+          });
+          job.save();
+      }
+  
+      res.send('Success');
+  }
 };
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,97 +3,89 @@ const jwt = require('jsonwebtoken');
 const UserService = require('../services/user.service');
 const logger = require('../helpers/logger');
 
-const login = async(req: any, res: any, next: any) => {
-
-  const user = await req.user;
-
-  console.log(user);
-  console.log(req);
-
-  const userData = {
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    profileUrl: user.profileUrl,
-    createdAt: user.createdAt,
-    updatedAt: user.updatedAt
-
-  }
-  const token = jwt.sign({
-    data: userData
-  }, generalConfig.token_secret, {
-    expiresIn: '2y'
-  });
-
-  res.send({
-    status: 200,
-    message: 'Login feito com sucesso!',
-    token
-  });
-};
-
-const getUser = async(req: any, res: any, next: any) => {
-  let user = await req.user;
-  res.send({status: 200, message: 'ok', data: {
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    profileUrl: user.profileUrl,
-    createdAt: user.createdAt,
-  }})
-}
-
-const createUser = async(req: any, res: any, next: any) => {
-  try {
-    const {firstName, lastName, password, email} = req.body
-
-    if(!validateEmail(email)) throw new Error('Invalid Email')
-    if(password.length < 3) throw new Error('Password is too small')
-    const profileUrl = `https://api.adorable.io/avatars/285/${email}`
-
-    const createdUser = await UserService.createUser(firstName, lastName, password, email, profileUrl)
-    const token = jwt.sign({
-      data: createdUser
-    }, process.env.TOKEN_SECRET, {
-      expiresIn: '2y'
-    });
-    res.send({...createdUser, token})
-  } catch (e: any) {
-    if (e.name === 'MongoError' && e.code === 11000) {
-      logger.error('Mongoose Error', e)
-      if("email" in e.keyValue){
-        res.status(400).send({ msg: `Email ${e.keyValue.email} already exist!`, status: 401, field: 'email' });
-      }
-    } else {
-      logger.error('Unable to Create User', e)
-      res.status(400).send({msg: e.message || 'Unable To Create User', status: 400})
-    }
-  }
-}
-
 function validateEmail(email: string) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
-const logout = async(req: any, res: any, next: any) => {
-  req.logout();
-  req.session.destroy(function (err: any) {
-    if (!err) {
-      res.status(200).clearCookie('connect.sid', {path: '/'}).json({status: "Success"});
-    } else {
-      // handle error case...
-      console.log(res)
-    }
-
-  });
-}
-
 module.exports = {
-  login,
-  getUser,
-  createUser,
-  logout
+  login: async(req: any, res: any, next: any) => {
+
+    const user = await req.user;
+  
+    console.log(user);
+    console.log(req);
+  
+    const userData = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      profileUrl: user.profileUrl,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
+  
+    }
+    const token = jwt.sign({
+      data: userData
+    }, generalConfig.token_secret, {
+      expiresIn: '2y'
+    });
+  
+    res.send({
+      status: 200,
+      message: 'Login feito com sucesso!',
+      token
+    });
+  },
+  getUser: async(req: any, res: any, next: any) => {
+    let user = await req.user;
+    res.send({status: 200, message: 'ok', data: {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      profileUrl: user.profileUrl,
+      createdAt: user.createdAt,
+    }})
+  },
+  createUser: async(req: any, res: any, next: any) => {
+    try {
+      const {firstName, lastName, password, email} = req.body
+  
+      if(!validateEmail(email)) throw new Error('Invalid Email')
+      if(password.length < 3) throw new Error('Password is too small')
+      const profileUrl = `https://api.adorable.io/avatars/285/${email}`
+  
+      const createdUser = await UserService.createUser(firstName, lastName, password, email, profileUrl)
+      const token = jwt.sign({
+        data: createdUser
+      }, process.env.TOKEN_SECRET, {
+        expiresIn: '2y'
+      });
+      res.send({...createdUser, token})
+    } catch (e: any) {
+      if (e.name === 'MongoError' && e.code === 11000) {
+        logger.error('Mongoose Error', e)
+        if("email" in e.keyValue){
+          res.status(400).send({ msg: `Email ${e.keyValue.email} already exist!`, status: 401, field: 'email' });
+        }
+      } else {
+        logger.error('Unable to Create User', e)
+        res.status(400).send({msg: e.message || 'Unable To Create User', status: 400})
+      }
+    }
+  },
+  logout: async(req: any, res: any, next: any) => {
+    req.logout();
+    req.session.destroy(function (err: any) {
+      if (!err) {
+        res.status(200).clearCookie('connect.sid', {path: '/'}).json({status: "Success"});
+      } else {
+        // handle error case...
+        console.log(res)
+      }
+  
+    });
+  }
 };
