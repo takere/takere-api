05c5e81f9d7104b7f02f13db899ceeefc799964a
William Niemiec <williamniemiec@hotmail.com>
2022-09-05 15:14:33 -0300

    Node types - suffix NODE removed

--- src/services/nodes/board-nodes.ts
+++ src/services/nodes/board-nodes.ts
@@ -1,10 +1,10 @@
 const boardNodes: string[] = [
-  'CONDITIONAL_NODE',
-  'EXPLANATION_NODE',
-  'ORIENTATION_NODE',
-  'MEDICATION_CONTROL_NODE',
-  'QUIZ_NODE',
-  'REMINDER_NODE'
+  'CONDITIONAL',
+  'EXPLANATION',
+  'ORIENTATION',
+  'MEDICATION_CONTROL',
+  'QUIZ',
+  'REMINDER'
 ];
 
 export default boardNodes;
